b9d4c7b87a4bd7a8b1c6c8536c0b3059f6e310af
ani <anna.unton@yahoo.com>
2017-12-03 18:33:28 +0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,6 @@
 // scripts.js
 
-var button = document.getElementById('get-joke');
-var paragraph = document.getElementById('joke');
-var url = 'http://api.icndb.com/jokes/random';
-
-getJoke();
-
-button.addEventListener ('click', function() {
-	getJoke();
-});
-
-function getJoke() {
-var xhr = new XMLHttpRequest();
-xhr.open ('GET', url); 
-
-xhr.addEventListener('load', function() {
-	var response = JSON.parse(xhr.response);
-	paragraph.innerHTML = response.value.joke;
-});
-
-xhr.send();
-
-};
-
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 
